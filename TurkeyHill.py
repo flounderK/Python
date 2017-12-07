5918b65dd8fc48899a24dab029587a192663a9cc
Clif Wolfe <34320249+flounderK@users.noreply.github.com>
2017-12-07 17:58:37 -0500

    Update TurkeyHill.py
    Wrote into reusable and exportable function

--- TurkeyHill.py
+++ TurkeyHill.py
@@ -9,16 +9,16 @@ import requests
 import re
 
 
-store_number = '01400433'
-#the website uses angular js
-#here is the url for the data it calls
-url = 'https://www.turkeyhillstores.com/store?store='
-s = requests.session()
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)'+
-           'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 '+
-           'Safari/537.36'}
-#original page url
-#url = 'https://www.turkeyhillstores.com/storeHours?store=01400433'
-r = s.get(url + store_number, headers=headers)
-k = re.search('\"storeServices\"\:\{[\W\w]+?\}', str(r.content))
-print(k[0])
+#store_number = '01400433'
+def query_store_services(store_number):
+           #the website uses angular js
+           #here is the url for the data it calls
+           url = 'https://www.turkeyhillstores.com/store?store='
+           headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)'+
+                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 '+
+                      'Safari/537.36'}
+           r = requests.get(url + store_number, headers=headers)
+           services = re.search('(?<=\"storeServices\"\:)\{[^}]+?\}', str(r.content))[0]
+           return services
+#usage:
+#query_store_services('01400433')
