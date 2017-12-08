b006aa84b486614a9c4dbdc47a456d1c13a46f90
Clif Wolfe <34320249+flounderK@users.noreply.github.com>
2017-12-07 20:03:09 -0500

    Add files via upload

---
+++ css.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 13 10:54:37 2017
+
+@author: Cliff.Wolfe
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+def get_css(soup, session, url_base):
+    
+    css_links = soup.find_all("link", attrs={"type": "text/css"})
+    i = 0
+    for link in css_links:
+        
+        link['href'] = re.sub("\.\.\/", "", link['href'])
+        if link['href'][0] == '/':
+            link['href'] = link['href'][1:]
+    
+        
+        file_location = url_base + link['href']
+        
+        r = session.get(file_location)
+        if r.status_code == 200:
+            new_filename = "css" + str(i) + ".css"
+            with open(new_filename, 'wb') as f:
+                f.write(r.content) 
+        
+        link['href'] = new_filename
+        i = i+1
+   
+    return css_links
+
+def get_url_base(url):
+    base = re.match("(\w){,}://[(\w)(\W)]{,}/", url)[0]
+    return base
+
+
+
+#url = 'http://srvsp01:8020/configurations.do'
+#s = requests.session()
+#r = s.get(url)
+#soup = BeautifulSoup(r.content, "lxml")
+#url_base = get_url_base(url)
+#get_css(soup, s, url_base)
