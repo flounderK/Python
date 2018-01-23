08474dee1f9e4d72e9419e8edabc6162beb85caf
Clif Wolfe <34320249+flounderK@users.noreply.github.com>
2018-01-23 16:29:16 -0500

    Update deck.py

--- deck.py
+++ deck.py
@@ -7,7 +7,10 @@ class card:
 		self.value = value
 
 	def display(self):
-		return self.__dict__
+		result =(str(self.__dict__['value']) + " of " +
+				 str(self.__dict__['suit']))
+		return result
+
 
 class deck(list):
 	"""A representation of a deck of playing cards"""
@@ -24,24 +27,53 @@ class deck(list):
 		shuffle(self)
 
 	def draw(self):
-		return self.pop().__dict__
+		return self.pop()
 
-	def view_deck(self):
-		view = list()
-		for i in self:
-		 	view.append(i.__dict__)
-		return view
+	def reset(self):
+		self.clear()
+		self.__init__()
+
+	# def view_deck(self):
+	# 	view = list()
+	# 	for i in self:
+	# 	 	view.append(i.__dict__)
+	# 	return view
 
 
 class player:
-	def __init__(self):
+	def __init__(self,name=None, cash=500):
 		self.hand = list()
+		self.cash = cash
+		self.current_bet = 0
 
-	def __look_at_hand(self):
+	def bet(self, cash):
+		if cash > self.cash:
+			return "You dont have that much money"
+
+		self.cash = self.cash - cash
+		self.current_bet = self.current_bet + cash
+
+	def add_money(self, amount):
+		self.cash = self.cash + amount
+
+	def look_at_hand(self):
 		view=list()
 		for i in self.hand:
-			view.append(i.__dict__)
+			view.append(i.display())
 		return view
 
 	def draw(self, deck):
-		pass
+		self.hand.append(deck.draw())
+
+	def return_cards(self):
+		self.hand.clear()
+
+class game:
+	def __init__(self):
+		self.played_cards = list()
+		self.pot = 0
+		self.current_bid = 10
+		self.players = list()
+
+	def add_player(self, player):
+		self.players.append(player)
