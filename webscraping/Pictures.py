59e5f67c2b6d09e8c360b42086b61d45a8c3a3d7
flounderK <flounderK@github.com>
2018-01-25 19:31:54 -0500

    Rewrote a bit of Pictures.py to resolve a few errors, reduce overhead from regex compilation, allow kwargs to be passed, and replace requests.get with get_retry

--- webscraping/Pictures.py
+++ webscraping/Pictures.py
@@ -8,60 +8,87 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-def get_url_base(url):
-    #return first part of url, no forward slash at the end
-    base = re.match("(\w+://|[^/])[^/]+(?=/)*", url)[0]
-    return base
+def get_retry(url, max_retries=10, session=None, **kwargs):
+
+    _url = url
+    _max_retries = max_retries
+    if session != None:
+        s=session
+    else:
+        s=requests.session()
+    retries = 0
+    finished = False
+    while finished != True:
+        try:
+            r = s.get(_url, **kwargs)
+
+        except requests.ConnectionError as e:
+            print("No response")
+            r = requests.Response
+            r.status_code = 0
+        except requests.exceptions.ReadTimeout as e:
+            print("Timeout")
+            r = requests.Response
+            r.status_code = 0
+        #raise an error here
+
+        if r.status_code == 200:
+            finished = True
+
+        retries = retries + 1
+
+        if retries >= _max_retries:
+            finished = True
+
+    return r
+
 
 #start of function
-def download_images(url, session, soup, default_filetype='.jpg'):
-    _soup = soup
-    _url = url
-    _session = session
-    for img in _soup.find_all("img"):
-        
+def download_images(url, session, soup, default_filetype='.jpg', **kwargs):
+
+    #regex compiled ahead of time
+    base_url_regex = re.compile("(\w+://|[^/])[^/]+")
+    img_name_regex = re.compile("(?<=/)[^/]+$")
+    ext_regex = re.compile("\.\w{3,4}$")
+    for img in soup.find_all("img"):
+
         #get_url_base makes sure that there is no forward slash at the end
-        url_base = get_url_base(_url)
-        
+        url_base = re.match(base_url_regex, url)[0]
+
         #making sure that we get the url for the image correct
-        if img['src'][0] == '/':    
+        if img['src'][0] == '/':
             imageurl = url_base + img['src']
         elif img['src'][0] == '.':
             imageurl = url_base + '/' + img['src']
         else:
             imageurl = img['src']
-                
+
         #getting image name
-        image_name = re.search("(?<=/)[^/]+$", img['src'])[0]
-        extension = re.search("\.\w{3,4}$", image_name)
+        image_name = re.search(img_name_regex, img['src'])[0]
+        if '?' in image_name:
+            image_name = image_name.split('?')[0]
+
+        #Here the match of the regex search does not need to be selected
+        #because we only care if it is missing
+        extension = re.search(ext_regex, image_name)
         if extension == 'None':
-            #"Image is being called with a script," +
-            #" naming image after token and defaulting to jpg"
+            #Image is most likely being called with a script,
+            #default to a different filetype
             image_name = image_name + default_filetype
-        
-        #this next chunk can be removed after error function is tested and used
-        r = _session.get(imageurl, timeout = 5)
+
+        r = get_retry(session=session, url=imageurl, timeout = 5, **kwargs)
         if r.status_code == 200:
             with open(image_name, 'wb') as f:
                 f.write(r.content)
-        
-        img['src'] = image_name
-    return soup
+        else:
+            return r.status_code
 
-#usage:            
+        img['src'] = image_name
+    #return soup
+
+#usage:
 #url = 'https://pixabay.com/'
 #s = requests.session()
 #r = s.get(url)
 #soup = BeautifulSoup(r.content, "lxml")
 #download_images(url, s, soup)
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
